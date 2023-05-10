7006ccfb2dedd6110b4f925df90affbbf3fdf5a2
Jeroen <63124598+JustJerbotPLAYZ@users.noreply.github.com>
2023-05-10 23:18:13 +0200

    Started work on home page, made functionality to pull data from database

--- content/home.php
+++ content/home.php
@@ -1,1 +1,24 @@
-<p>home.php</p>
+<div class=" text-center">
+    <div class="row background-img align-items-center">
+        <div class="child">
+            <image class="background-img" src="img/beginfoto-stranden-zeeland-1997112101-660x330.jpg">
+        </div>
+        <div class="child blurbackground cover-img">
+            <?php
+            $sql = "SELECT titel, pagina, tekst FROM teksten";
+
+            // Execute the query and fetch the results
+            $sys = $conn->query($sql);
+            $results = $sys->fetchAll(PDO::FETCH_ASSOC);
+
+            // Loop through the results and print the desired columns
+            foreach ($results as $row) {
+                if ($row['pagina'] == $link) {
+                    echo "<h1 class='ps-4 pe-4 cover-img'>" . $row['titel'] . "</h1>";
+                    echo "</div></div><div class='row'>";
+                    echo "<p class='ps-4 pe-4'>" . $row['tekst'] . "</p>";
+                }
+            }
+            ?>
+        </div>
+    </div>
--- content/vakantiehuizen.php
+++ content/vakantiehuizen.php
@@ -1,1 +1,20 @@
 <p>vakantiehuizen.php</p>
+<div class="row">
+    <div class="col text-center">
+        <?php
+        $sql = "SELECT titel, pagina, tekst FROM teksten";
+
+        // Execute the query and fetch the results
+        $sys = $conn->query($sql);
+        $results = $sys->fetchAll(PDO::FETCH_ASSOC);
+
+        // Loop through the results and print the desired columns
+        foreach ($results as $row) {
+            if ($row['pagina'] == $link) {
+                echo "<h1 class='ps-4 pe-4'>" . $row['titel'] . "</h1>";
+                echo "<p class='ps-4 pe-4'>" . $row['tekst'] . "</p>";
+            }
+        }
+        ?>
+    </div>
+</div>
