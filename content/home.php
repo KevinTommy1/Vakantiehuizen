5cbd15c45b488c166cf4ebf8ef5bb7caa086ee16
DESKTOP-L4V3QNJ\jdsuu <238632@student.scalda.nl>
2023-05-24 12:03:43 +0200

    Home pagina klaar met * fancy * styling, en tekst opgeschoont bij vakantiehuizen verhuur (wat de fuck dacht ik toen ik de text maakte, waarom gebruikte ik zoveel divs en cols?????????)

--- content/home.php
+++ content/home.php
@@ -1,4 +1,4 @@
-<div class=" text-center">
+<div class="text-center">
     <div class="row background-img align-items-center">
         <div class="child">
             <image class="background-img" src="img/beginfoto-stranden-zeeland-1997112101-660x330.jpg">
@@ -18,18 +18,60 @@
             ?>
         </div>
     </div>
-    <div class="row">
-        <div class="col">
-            <div class="container">
-                <i class="fa-solid fa-check-double d-flex"></i><p class="d-flex">test</p>
+    <div class="row pt-4">
+        <div class="col-md"></div>
+        <div class="col-md">
+            <div class="cont-subtext">
+                <div class="inline-flex">
+                    <ul class="ul-same-row reset">
+                        <li>
+                            <i class="fa-solid fa-check-double fa-2xl"></i>
+                        </li>
+                        <li>
+                            <p class="reset">Goedkoop</p>
+                        </li>
+                    </ul>
+                </div>
+                <p class="subtext">Wij bieden de goedkoopste service met de hoogste kwaliteit in zeeland!</p>
             </div>
         </div>
+        <div class="col-md">
+            <div class="cont-subtext">
+                <div class="inline-flex">
+                    <ul class="ul-same-row reset">
+                        <li>
+                            <i class="fa-solid fa-check-double fa-2xl"></i>
+                        </li>
+                        <li>
+                            <p class="reset">Duurzaam</p>
+                        </li>
+                    </ul>
+                </div>
+                <p class="subtext">Onze huisjes zijn uitstoot vrij en op een combi van zonne- en windenergie.</p>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="cont-subtext">
+                <div class="inline-flex">
+                    <ul class="ul-same-row reset">
+                        <li>
+                            <i class="fa-solid fa-check-double fa-2xl"></i>
+                        </li>
+                        <li>
+                            <p class="reset">Écht zeeuws</p>
+                        </li>
+                    </ul>
+                </div>
+                <p class="subtext"></p>
+            </div>
+        </div>
+        <div class="col-md"></div>
     </div>
-    <div class='row'>
+    <div class="container cont-subtext-2 mt-5">
         <?php
         foreach ($results as $row) {
             if ($row['pagina'] == $link) {
-                echo "<p class='ps-4 pe-4'>" . $row['tekst'] . "</p>";
+                echo "<p class='ps-4 pe-4 pt-2 reset'>" . $row['tekst'] . "</p>";
             }
         }
         ?>
--- content/vakantiehuizen.php
+++ content/vakantiehuizen.php
@@ -1,19 +1,21 @@
 <p>vakantiehuizen.php</p>
-<div class="row text-center">
+<div class="cont-subtext-2 wordkleinerkutding">
+    <div class="row text-center">
 
-    <?php
-    $sql = "SELECT titel, pagina, tekst FROM teksten";
+        <?php
+        $sql = "SELECT titel, pagina, tekst FROM teksten";
 
-    // Execute the query and fetch the results
-    $sys = $conn->query($sql);
-    $results = $sys->fetchAll(PDO::FETCH_ASSOC);
+        // Execute the query and fetch the results
+        $sys = $conn->query($sql);
+        $results = $sys->fetchAll(PDO::FETCH_ASSOC);
 
-    // Loop through the results and print the desired columns
-    foreach ($results as $row) {
-        if ($row['pagina'] == $link) {
-            echo "<div class='col-lg'></div><div class='col-lg-6'><h1 class='ps-4 pe-4'>" . $row['titel'] . "</h1></div><div class='col-lg'></div></div><div class='row text-center'>";
-            echo "<div class='col-lg'></div><div class='col-lg-6'><p class='ps-4 pe-4'>" . $row['tekst'] . "</p></div><div class='col-lg'></div>";
+        // Loop through the results and print the desired columns
+        foreach ($results as $row) {
+            if ($row['pagina'] == $link) {
+                echo "<div class='col-lg'><h1 class='ps-4 pe-4'>" . $row['titel'] . "</h1></div></div><div class='row text-center'>";
+                echo "<div class='col-lg'><p class='ps-4 pe-4'>" . $row['tekst'] . "</p></div>";
+            }
         }
-    }
-    ?>
+        ?>
+    </div>
 </div>
