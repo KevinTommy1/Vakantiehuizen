584d32ade5d99c45cb5855ff1b04390a304a22d4
DESKTOP-L4V3QNJ\jdsuu <238632@student.scalda.nl>
2023-05-26 21:30:40 +0200

    Iets toegevoegd ofzo, geen idee tbh

--- content/vakantiehuizen.php
+++ content/vakantiehuizen.php
@@ -19,26 +19,28 @@
     </div>
 </div>
 <div class="row">
-<div class="row p-5">
-    <?php
-    $sql = "SELECT personen, prijs, huis, omschrijving_small, omschrijving FROM huizen";
+    <div class="row p-5">
+        <?php
+        $sql = "SELECT personen, prijs, huis, omschrijving_small, omschrijving FROM huizen";
 
-    $sys = $conn->query($sql);
-    $results = $sys->fetchAll(PDO::FETCH_ASSOC);
+        $sys = $conn->query($sql);
+        $results = $sys->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($results as $row) {
-        echo "
-    <div class='col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
-        <div class='card m-auto mt-3'>
-            <div class='card-body'>
-                <img class='card-img-top' src='img/huisjes/bos_lodge/bos_lodge.png' alt'img'>
-                <h5 class='card-title'>".$row['huis']."</h5>
-                <h6 class='card-subtitle mb-2 text-muted'>€".$row['prijs']."</h6>
-                <p class='card-text'>".$row['omschrijving_small']."</p>
-                <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>Meer informatie</button>
-                <a href='#' class='btn btn-primary'>Order</a>
+        foreach ($results as $row) {
+            echo "
+        <div class='col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
+            <div class='card m-auto mt-3'>
+                <div class='card-body'>
+                    <img class='card-img-top' src='img/huisjes/bos_lodge/bos_lodge.png' alt'img'>
+                    <h5 class='card-title'>" . $row['huis'] . "</h5>
+                    <h6 class='card-subtitle mb-2 text-muted'>€" . $row['prijs'] . "</h6>
+                    <p class='card-text'>" . $row['omschrijving_small'] . "</p>
+                    <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>Meer informatie</button>
+                    <a href='#' class='btn btn-primary'>Order</a>
+                </div>
             </div>
-        </div>
-    </div>";
-    }
-    ?>
+        </div>";
+        }
+        ?>
+    </div>
+</div>
