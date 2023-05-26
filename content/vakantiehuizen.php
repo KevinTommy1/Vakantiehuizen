3d351f766efac333e09e90fd08cc64e54b6223aa
Jeroen <63124598+JustJerbotPLAYZ@users.noreply.github.com>
2023-05-26 23:05:51 +0200

    Fixed them oopsies sksksk

--- content/vakantiehuizen.php
+++ content/vakantiehuizen.php
@@ -21,25 +21,50 @@
 <div class="row">
     <div class="row p-5">
         <?php
-        $sql = "SELECT personen, prijs, huis, omschrijving_small, omschrijving FROM huizen";
+        $sql = "SELECT h.personen, h.prijs, h.huis, h.omschrijving_small, h.omschrijving, h.id, a.huis_id, a.afbeelding
+        FROM huizen h
+        LEFT JOIN afbeeldingen a ON a.huis_id = h.id
+        ORDER BY h.huis;
+        ";
 
         $sys = $conn->query($sql);
         $results = $sys->fetchAll(PDO::FETCH_ASSOC);
 
         foreach ($results as $row) {
             echo "
-        <div class='col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
-            <div class='card m-auto mt-3'>
-                <div class='card-body'>
-                    <img class='card-img-top' src='img/huisjes/bos_lodge/bos_lodge.png' alt'img'>
+        <div class='col-md-6 col-lg-4 col-xl-3 col-xxl-3 mt-3'>
+            <div class='card m-auto h-100'>
+                <div class='card-header'>
+                    <img class='card-img-top mt-2' src='".$row['afbeelding']."' alt'foto ".$row['huis']."'>
                     <h5 class='card-title'>" . $row['huis'] . "</h5>
+                </div>
+                <div class='card-body'>
                     <h6 class='card-subtitle mb-2 text-muted'>€" . $row['prijs'] . "</h6>
                     <p class='card-text'>" . $row['omschrijving_small'] . "</p>
-                    <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>Meer informatie</button>
-                    <a href='#' class='btn btn-primary'>Order</a>
+                </div>
+                <div class='card-footer'>
+                    <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modal" . $row['id'] . "'>Meer informatie</button>
                 </div>
             </div>
-        </div>";
+        </div>
+        <div class='modal fade ' id='modal" . $row['id'] . "' tabindex='-1' aria-labelledby='modalLabe" . $row['id'] . "' aria-hidden='true'>
+        <div class='modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg'>
+            <div class='modal-content'>
+                <div class='modal-header'>
+                    <h5 class='modal-title' id='modalLabel" . $row['id'] . "'>" . $row['huis'] . "</h5>
+                    <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                </div>
+                <div class='modal-body'>
+                    <p>
+                        " . $row['omschrijving'] . "
+                    </p>
+                </div>
+                <div class='modal-footer'>
+                    <button type='button' class='btn btn-primary' data-bs-dismiss='modal'>Close</button>
+                </div>
+            </div>
+        </div>
+    </div>";
         }
         ?>
     </div>
