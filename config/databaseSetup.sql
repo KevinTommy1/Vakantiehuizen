1632223f8aad25a8789ac6fb48bb987ff1f63404
Jeroen <63124598+JustJerbotPLAYZ@users.noreply.github.com>
2023-05-28 23:45:02 +0200

    I have gone completely insane and no one can stop me, I am the most powerfull person on this earth please this is a cry for help save me I dont want to code anymore php my beloved tho <3

---
+++ config/databaseSetup.sql
@@ -0,0 +1,68 @@
+drop database if exists vakantiehuizen;
+create database if not exists vakantiehuizen;
+
+use vakantiehuizen;
+
+create table teksten (
+id int primary key AUTO_INCREMENT,
+pagina varchar(255),
+titel varchar(255),
+tekst varchar(1000) );
+
+create table huizen (
+id int primary key AUTO_INCREMENT,
+personen int,
+prijs float,
+huis varchar(255),
+omschrijving_small varchar(200),
+omschrijving varchar(2000) );
+
+create table afbeeldingen(
+id int primary key auto_increment,
+huis_id int,
+afbeelding varchar(100),
+FOREIGN KEY (huis_id) REFERENCES huizen(id)
+);
+
+
+INSERT INTO teksten (pagina,titel,tekst)
+VALUES ('home','Welkom bij Zilt & Zo Vakantieverblijven, 
+<br>dé plek voor een onvergetelijke vakantie in het prachtige Zeeland!',
+'Onze vakantiehuisjes bieden de perfecte uitvalsbasis om te genieten van de Zeeuwse stranden, duinen, en pittoreske dorpjes.  
+Of je nu op zoek bent naar rust en ontspanning, of juist naar avontuur en actie, bij ons vind je het allemaal. Onze huisjes zijn   
+met zorg en aandacht ingericht, zodat je je meteen thuis voelt. Neem gerust een kijkje op onze website en ontdek ons aanbod.  
+We hopen je snel te mogen verwelkomen bij Zilt & Zo Vakantieverblijven!');
+INSERT INTO teksten (pagina,titel,tekst)
+VALUES ('vakantiehuizen','Verhuur',"Onze vakantiehuisjes bieden alles wat je nodig hebt voor een heerlijk verblijf aan de Zeeuwse kust. 
+Elk huisje is met zorg en aandacht ingericht, zodat je je meteen thuis voelt. Onze huisjes variëren van knusse studio's tot ruime villa's 
+en bieden plaats aan 2 tot 16 personen. Elk huisje beschikt over een eigen keuken, badkamer en woonkamer, en is voorzien van moderne voorzieningen 
+zoals gratis Wi-Fi en een flatscreen-tv. Bovendien hebben onze huisjes een eigen terras of balkon, waar je kunt genieten van het prachtige uitzicht op de omgeving. 
+Of je nu een romantisch weekendje weg wilt plannen of een gezinsvakantie, bij Zilt & Zo Vakantieverblijven vind je het perfecte vakantiehuisje voor elke gelegenheid. 
+Neem een kijkje bij ons aanbod en boek vandaag nog jouw droomvakantie in Zeeland!");
+
+insert into huizen (personen, prijs, huis, omschrijving_small, omschrijving) values (8,95.00,"Zandkreek","Omschrijving Zandkreek klein","Omschrijving Zandkreek groot");
+insert into huizen (personen, prijs, huis, omschrijving_small, omschrijving) values (12,120.00,"Oosterschelde","Omschrijving Oosterschelde klein","Omschrijving Oosterschelde groot");
+insert into huizen (personen, prijs, huis, omschrijving_small, omschrijving) values (12,110.50,"Grevelingen","Omschrijving Grevelingen klein","Omschrijving Grevelingen groot");
+insert into huizen (personen, prijs, huis, omschrijving_small, omschrijving) values (16,135.95,"Westerschelde","Omschrijving Westerschelde klein","Omschrijving Westerschelde groot");
+
+insert into afbeeldingen(huis_id,afbeelding) values(1,"img/huisjes/8pers/8pers.png");
+insert into afbeeldingen(huis_id,afbeelding) values(1,"img/huisjes/8pers/8pers_bed.png");
+insert into afbeeldingen(huis_id,afbeelding) values(1,"img/huisjes/8pers/8pers_keuken.png");
+
+insert into afbeeldingen(huis_id,afbeelding) values(2,"img/huisjes/12pers/12pers_bed.png");
+insert into afbeeldingen(huis_id,afbeelding) values(2,"img/huisjes/12pers/12pers_keuken.png");
+insert into afbeeldingen(huis_id,afbeelding) values(2,"img/huisjes/12pers/12pers.png");
+
+insert into afbeeldingen(huis_id,afbeelding) values(3,"img/huisjes/10pers/10pers.png");
+insert into afbeeldingen(huis_id,afbeelding) values(3,"img/huisjes/10pers/10pers_bed.png");
+insert into afbeeldingen(huis_id,afbeelding) values(3,"img/huisjes/10pers/10pers_keuken.png");
+insert into afbeeldingen(huis_id,afbeelding) values(4,"img/huisjes/16pers/16pers.png");
+insert into afbeeldingen(huis_id,afbeelding) values(4,"img/huisjes/16pers/16pers_bed.png");
+insert into afbeeldingen(huis_id,afbeelding) values(4,"img/huisjes/16pers/16pers_keuken.png");
+
+
+
+SELECT h.personen, h.prijs, h.huis, h.omschrijving_small, h.omschrijving, h.id, a.huis_id, a.afbeelding
+        FROM huizen h
+        LEFT JOIN afbeeldingen a ON a.huis_id = h.id
+        ORDER BY h.huis
--- content/vakantiehuizen.php
+++ content/vakantiehuizen.php
@@ -20,52 +20,85 @@
 </div>
 <div class="row">
     <div class="row p-5">
-        <?php
-        $sql = "SELECT h.personen, h.prijs, h.huis, h.omschrijving_small, h.omschrijving, h.id, a.huis_id, a.afbeelding
-        FROM huizen h
-        LEFT JOIN afbeeldingen a ON a.huis_id = h.id
-        ORDER BY h.huis;
-        ";
+    <?php
+    $firstInArray = true;
+    $lastId = 0;
 
-        $sys = $conn->query($sql);
-        $results = $sys->fetchAll(PDO::FETCH_ASSOC);
+    $sql = "SELECT h.personen, h.prijs, h.huis, h.omschrijving_small, h.omschrijving, h.id, a.huis_id, a.afbeelding
+    FROM huizen h
+    LEFT JOIN afbeeldingen a ON a.huis_id = h.id
+    ORDER BY h.huis";
 
-        foreach ($results as $row) {
+    $sys = $conn->query($sql);
+    $results = $sys->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($results as $row) {
+        if ($row['id'] != $lastId) {
             echo "
-        <div class='col-md-6 col-lg-4 col-xl-3 col-xxl-3 mt-3'>
-            <div class='card m-auto h-100'>
-                <div class='card-header'>
-                    <img class='card-img-top mt-2' src='".$row['afbeelding']."' alt'foto ".$row['huis']."'>
-                    <h5 class='card-title'>" . $row['huis'] . "</h5>
-                </div>
-                <div class='card-body'>
-                    <h6 class='card-subtitle mb-2 text-muted'>€" . $row['prijs'] . "</h6>
-                    <p class='card-text'>" . $row['omschrijving_small'] . "</p>
-                </div>
-                <div class='card-footer'>
-                    <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modal" . $row['id'] . "'>Meer informatie</button>
-                </div>
-            </div>
+<div class='col-md-6 col-lg-4 col-xl-3 col-xxl-3 mt-3'>
+    <div class='card m-auto h-100'>
+        <div class='card-header'>
+            <img class='card-img-top mt-2' src='" . $row['afbeelding'] . "' alt='foto " . $row['huis'] . "'>
+            <h5 class='card-title'>" . $row['huis'] . "</h5>
         </div>
-        <div class='modal fade ' id='modal" . $row['id'] . "' tabindex='-1' aria-labelledby='modalLabe" . $row['id'] . "' aria-hidden='true'>
-        <div class='modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg'>
-            <div class='modal-content'>
-                <div class='modal-header'>
-                    <h5 class='modal-title' id='modalLabel" . $row['id'] . "'>" . $row['huis'] . "</h5>
-                    <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                </div>
-                <div class='modal-body'>
-                    <p>
-                        " . $row['omschrijving'] . "
-                    </p>
-                </div>
-                <div class='modal-footer'>
-                    <button type='button' class='btn btn-primary' data-bs-dismiss='modal'>Close</button>
-                </div>
-            </div>
+        <div class='card-body'>
+            <h6 class='card-subtitle mb-2 text-muted'>€" . $row['prijs'] . "</h6>
+            <p class='card-text'>" . $row['omschrijving_small'] . "</p>
         </div>
-    </div>";
-        }
-        ?>
+        <div class='card-footer'>
+            <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modal" . $row['id'] . "'>Meer informatie</button>
+        </div>
     </div>
+</div>
+<div class='modal fade' id='modal" . $row['id'] . "' tabindex='-1' aria-labelledby='modalLabel" . $row['id'] . "' aria-hidden='true'>
+    <div class='modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg'>
+        <div class='modal-content'>
+            <div class='modal-header'>
+                <h5 class='modal-title' id='modalLabel" . $row['id'] . "'>" . $row['huis'] . "</h5>
+                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+            </div>
+            <div class='modal-body'>
+                <div id='carousel" . $row['id'] . "' class='carousel slide'>
+                    <div class='carousel-inner'>";
+
+            foreach ($results as $picture) {
+                if ($picture['huis_id'] == $row['id'] && $picture['afbeelding'] != null) {
+                    echo "<div class='carousel-item";
+                    if ($firstInArray) {
+                        echo " active";
+                        $firstInArray = false;
+                    }
+                    echo "'>
+                            <img src='" . $picture['afbeelding'] . "' class='d-block w-100 h-50'>
+                        </div>";
+                }
+            }
+
+            echo "</div>
+                    <button class='carousel-control-prev' type='button' data-bs-target='#carousel" . $row['id'] . "' data-bs-slide='prev'>
+                        <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+                        <span class='visually-hidden'>Previous</span>
+                    </button>
+                    <button class='carousel-control-next' type='button' data-bs-target='#carousel" . $row['id'] . "' data-bs-slide='next'>
+                        <span class='carousel-control-next-icon' aria-hidden='true'></span>
+                        <span class='visually-hidden'>Next</span>
+                    </button>
+                </div>
+                <p>" . $row['omschrijving'] . "</p>
+            </div>
+            <div class='modal-footer'>
+                <button type='button' class='btn btn-primary' data-bs-dismiss='modal'>Close</button>
+            </div>
+        </div>
+    </div>
+</div>";
+            $lastId = $row['id'];
+        }
+    }
+?>
+
+    </div>
+
+
+</div>
 </div>
