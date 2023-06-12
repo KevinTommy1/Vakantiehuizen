e31ce930b85c942c2106f66571c7db815799b365
Kevin_Hamelink <240424@student.scalda.nl>
2023-06-12 11:19:35 +0200

    Admin pagina begin gemaakt

---
+++ content/admin.php
@@ -0,0 +1,27 @@
+<title>Contact</title>
+
+<body class="body_color">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="login">
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div>
+                    <h1 class="login_header">Login</h1>
+                </div>
+                <div class="loginForm">
+                    <form method="post">
+                        <input type="text" name="username" placeholder="username" class="login" required>
+                        <input type="password" name="wachtwoord" class="login" required>
+                        <div>
+                            <button class="button" name="submit">send</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
